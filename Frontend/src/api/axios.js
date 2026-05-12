748e7143ddd01d257d0721bbbfc855b25fb03ab9
PruthviDev <160571523+PruthviDev@users.noreply.github.com>
2026-05-12 11:08:42 +0530

    Update axios baseURL to include '/api' path

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-    baseURL:"https://product-management-2-h1c1.onrender.com"
+    baseURL:"https://product-management-2-h1c1.onrender.com/api"
 });
 API.interceptors.request.use((req)=>{
     const token = localStorage.getItem("token");
